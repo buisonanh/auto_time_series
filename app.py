b1f3cac4b30cfa679234559b7eefba9974ebae1c
Son Anh <sonanhbui2407@gmail.com>
2023-03-22 11:10:56 +0700

    ok

--- app.py
+++ app.py
@@ -24,7 +24,6 @@ if choice == "Upload":
     file = st.file_uploader("Upload your file here:")
     if file:
         df = pd.read_csv(file, index_col=None)
-        df["Date"] = pd.to_datetime(df["Date"], format="%Y%m%d")
         df.to_csv("sourcedata.csv", index=None)
         st.dataframe(df)
 
